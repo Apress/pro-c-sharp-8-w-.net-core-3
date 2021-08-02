916f7d7e80470ae46819ace24c0d2b051e9264e2
Philip Japikse <skimedic@gmail.com>
2021-08-02 12:13:04 -0400

    Correct property order for BulkCopy (this issue was resolved in the C# 9 with a change to the BulkCopy process)

--- Chapter_21/AutoLot.DAL/AutoLot.DAL/Models/Car.cs
+++ Chapter_21/AutoLot.DAL/AutoLot.DAL/Models/Car.cs
@@ -3,8 +3,8 @@
     public class Car
     {
         public int Id { get; set; }
-        public string Color { get; set; }
         public string Make { get; set; }
+        public string Color { get; set; }
         public string PetName { get; set; }
     }
 }
